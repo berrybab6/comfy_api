c934d8b0c496661648631675255468bc191f9717
berrybab6 <bedatuassefa@gmail.com>
2022-03-06 14:07:27 +0300

    updated Setting.py

--- comfy_api/settings.py
+++ comfy_api/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'comfy_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Comfy',
+        'USER': 'postgres',
+        'PASSWORD': 'new_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
