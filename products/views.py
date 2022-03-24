4d81697481b999822c6fcb7464cdb8959e829b2a
berrybab6 <bedatuassefa@gmail.com>
2022-03-24 12:00:39 +0300

    create newest/featured/category view

--- products/views.py
+++ products/views.py
@@ -1,17 +1,105 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
 # from html5lib import serialize
 from rest_framework import viewsets
-from .models import ItemSizeColor, Products
+from .models import ComfyProducts, ComfySale, Dimension, ItemSizeColor, ProductImages, Products, ShippingInfo
 from rest_framework import generics, permissions, status
 from django.http import JsonResponse
 
-from .serializers import ColorSizeSerializer, ProductSerializer
+from .serializers import ColorSizeSerializer, ComfyProductsSerializer, ComfySaleSerializer, DimensionSerializer, ProductImagesSerializer, ProductSerializer, ShippingInfoSerializer
 # Create your views here.
 
 class ProductListView(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Products.objects.all()
 
+    def get_queryset(self):
+        return super().get_queryset()
+
+class ComfyProductView(viewsets.ModelViewSet):
+    serializer_class = ComfyProductsSerializer
+    queryset = ComfyProducts.objects.all()
+
+    
+    def get_queryset(self):
+        return super().get_queryset()
+
+class FeaturedProductsView(generics.GenericAPIView):
+    serializer_class = ComfyProductsSerializer
+    queryset = ComfyProducts.objects.all()
+    permission_classes = [permissions.AllowAny, ]
+
+    def get(self, request):
+        prod = ComfyProducts.objects.filter(featured =True)
+        if prod:
+            ser = ComfyProductsSerializer(prod, many=True)
+            return JsonResponse({"products":ser.data})
+        else:
+            JsonResponse({"error":"No Products Found"})
+    def get_queryset(self):
+        return super().get_queryset()
+
+class NewestProductsView(generics.GenericAPIView):
+    serializer_class = ComfyProductsSerializer
+    queryset = ComfyProducts.objects.all()
+    permission_classes = [permissions.AllowAny, ]
+
+    def get(self, request):
+        zare = datetime.today()
+        
+            
+        d = datetime.today() - timedelta(days=7)
+
+        prod = ComfyProducts.objects.filter(date_created__range=[d, zare])
+        if prod:
+            ser = ComfyProductsSerializer(prod, many=True)
+            return JsonResponse({"products":ser.data})
+        else:
+            return JsonResponse({"error":"No Products Found"})
+    def get_queryset(self):
+        return super().get_queryset()
+
+
+class CategoryProductsView(generics.GenericAPIView):
+    serializer_class = ComfyProductsSerializer
+    queryset = ComfyProducts.objects.all()
+    permission_classes = [permissions.AllowAny, ]
+
+    def get(self, request,category=None):
+        prod = ComfyProducts.objects.filter(prod_category=category)
+        if prod:
+            ser = ComfyProductsSerializer(prod, many=True)
+            return JsonResponse({"products":ser.data})
+        else:
+            return JsonResponse({"error":"No Products Found"})
+    def get_queryset(self):
+        return super().get_queryset()
+
+class ComfySaleView(viewsets.ModelViewSet):
+    serializer_class = ComfySaleSerializer
+    queryset = ComfySale.objects.all()
+    
+    def get_queryset(self):
+        return super().get_queryset()
+
+class DimensionView(viewsets.ModelViewSet):
+    serializer_class = DimensionSerializer
+    queryset = Dimension.objects.all()
+    
+    def get_queryset(self):
+        return super().get_queryset()
+
+class ShippingInfoView(viewsets.ModelViewSet):
+    serializer_class = ShippingInfoSerializer
+    queryset = ShippingInfo.objects.all()
+    
+    def get_queryset(self):
+        return super().get_queryset()
+
+class ProductImagesView(viewsets.ModelViewSet):
+    serializer_class = ProductImagesSerializer
+    queryset = ProductImages.objects.all()
+    
     def get_queryset(self):
         return super().get_queryset()
 
@@ -22,6 +110,12 @@ class ItemSizeListView(viewsets.ModelViewSet):
     
     def get_queryset(self):
         return super().get_queryset()
+
+
+# class ProductsDetailView(generics.GenericAPIView):
+
+
+    
 class ProductsDetailView(generics.GenericAPIView):
     queryset = [ItemSizeColor.objects.all(),Products.objects.all()]
     
@@ -41,6 +135,8 @@ class ProductsDetailView(generics.GenericAPIView):
                 return JsonResponse({"error":"There is no other detail with this prodID"}, status=status.HTTP_404_NOT_FOUND)
         else:
             return JsonResponse({"error":"Product Doesnot exist"}, status=status.HTTP_404_NOT_FOUND)
+
+
 
     # def get_queryset(self):
     #     return super().get_queryset()
