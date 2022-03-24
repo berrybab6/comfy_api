467624ba1c8d8194e744e6a2cf3f496ed4ea4bf9
berrybab6 <bedatuassefa@gmail.com>
2022-03-24 12:02:39 +0300

    update setting

--- comfy_api/settings.py
+++ comfy_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zn=pbi7m2$#bsyv4@-aaayu!vhvt^pqmxwjcs3g9bfoj5^6%5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
