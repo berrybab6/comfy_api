1995b9db11bda25bc58215850fb9d57f6fe4073a
berrybab6 <bedatuassefa@gmail.com>
2022-05-08 20:38:44 +0300

    update products serializer

--- comfy_api/products/serializers.py
+++ comfy_api/products/serializers.py
@@ -1,3 +1,5 @@
+from account.serializers import UserSerializers
+from .models import User
 from rest_framework import serializers
 from .models import ComfyProducts, ComfySale, Dimension, FavoriteProduct, ItemSizeColor, ProductImages,  ShippingInfo
 
@@ -54,4 +56,21 @@ class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = FavoriteProduct
         fields = "__all__"
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        try:
+            wished_item = ComfyProducts.objects.get(id=data['wished_item'])
+            data['wished_item'] = ComfyProductsSerializer(wished_item).data
+
+        except ComfyProducts.DoesNotExist:
+            wished_item = None
+        
+        # try:
+        #     user = User.objects.get(id=data['user_id'])
+        #     data['user'] = UserSerializers(user).data
+            
+        # except User.DoesNotExist:
+        #     user = None
+        
+        return data  
         
