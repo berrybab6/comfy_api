9030d79fceae1bf2032708f0fbe1a0c2b15e4201
mishassefa <bedatuassefa@gmail.com>
2022-08-12 14:03:31 +0300

    update root path

--- comfy_api/comfy_api/urls.py
+++ comfy_api/comfy_api/urls.py
@@ -47,5 +47,5 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('',include("products.urls",namespace="products"))
+    path('',include("products.urls"))
 ]
